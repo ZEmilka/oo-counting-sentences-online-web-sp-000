352a68fa370a0354b1ab2199564765ffe66822fc
Emilia Ziemichód <emifrompoland@gmail.com>
2019-04-17 04:34:09 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class String
 
-  def sentence?(string)
-    if string.end_with?(".")
+  def sentence?(sentence)
+    if sentence.end_with?(".")
     else
       return false
     end
